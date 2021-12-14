2b4031f6f25c9bd04b6d2ed9587ed7af12d8aa0f
laux2laux@gmail.com <laux2laux@gmail.com>
2021-12-14 20:47:29 -0300

    corrige rota

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -1,5 +1,6 @@
 const { v4: uuid } = require('uuid')
 const User = require('../model/User')
+const bcrypt = require('bcrypt')
 
 interface IUser {
   _id: string
@@ -15,6 +16,7 @@ interface Itask {
 }
 
 module.exports = {
+  
   async getUsers(request: any, response: any) {
     try {
       const users = await User.find()
@@ -26,20 +28,21 @@ module.exports = {
 
   async getUser(request: any, response: any) {
     const { userName, password } = request.body
-
     try {
       if (!userName || !password) {
         return response.status(400).json({ error: 'Missing name or password' })
       } else {
         const users = await User.find()
 
-        users.forEach((user: IUser) => {
-          if (user.userName === userName) {
-            return response.status(200).json({ user })
-          } else {
-            return response.status(400).json({ error: 'User does not exist' })
-          }
-        })
+        const user = users.find((user: IUser) => user.userName === userName)
+
+        if (user === null) {
+          return response.status(400).send('Cannot find user')
+        } else if (await bcrypt.compare(password, user.password)) {
+          return response.status(200).json({ user })
+        } else {
+          return response.status(400).send('Cannot find user')
+        }
       }
     } catch (error: any) {
       response.status(500).json({ error: error.message })
@@ -49,18 +52,36 @@ module.exports = {
   async createUser(request: any, response: any) {
     const { userName, password, tasks } = request.body
 
-    const task = new User({
-      _id: uuid(),
-      userName: userName,
-      password: password,
-      tasks: tasks
-    })
-    try {
-      await task.save()
+    const users = await User.find()
 
-      return response.status(201).json({ message: 'user created successfully' })
-    } catch (error: any) {
-      return response.status(400).json({ error: error.message })
+    let userCreationFlag = false
+
+    users.forEach((user: IUser) => {
+      if (user.userName === userName) {
+        console.log(userName)
+        userCreationFlag = true
+      }
+    })
+
+    if (!userName || !password || !tasks) {
+      return response.status(400).json({ error: 'Missing name, password or tasks' })
+    }
+    if (userCreationFlag) {
+      return response.status(400).json({ rror: 'This user already exists' })
+    } else {
+      const hashedPassword = await bcrypt.hash(request.body.password, 10)
+      const task = new User({
+        _id: uuid(),
+        userName: userName,
+        password: hashedPassword,
+        tasks: tasks
+      })
+      try {
+        await task.save()
+        return response.status(201).json({ message: 'user created successfully' })
+      } catch (error: any) {
+        return response.status(400).json({ error: error.message })
+      }
     }
   }
 }
--- src/routes/trelloRoutes.ts
+++ src/routes/trelloRoutes.ts
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const userController = require('../controller/userController')
-const userMidllewares = require('../midllewares/userMidllewares')
+const userMidllewares = require('../midllewares/userMiddlewares')
 
 router.get('/users', userController.getUsers)
 
 router.get('/user', userController.getUser)
 
-router.post('/createUser', userMidllewares.validateUserCreation, userController.createUser)
+router.post('/createUser', userController.createUser)
 
 export = router
